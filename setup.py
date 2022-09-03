1eed689ed589411c3b826e7529a0050a7c9173cf
futagoya <firdauswnn@gmail.com>
2022-09-03 18:52:52 +0700

    release to pypi at https://pypi.org/project/BMKG-Latest/0.4/

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
 
     name="BMKG-Latest",
-    version="0.3",
+    version="0.4",
     author="Firdaus W N",
     author_email="<firdauswnn@gmail.com>",
     description="This package will guide us to receive the latest Earthquake Information from BMKG Indonesia",
