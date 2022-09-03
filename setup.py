fe2824fa75b7e46afcebff3bbf495ad0154de685
futagoya <firdauswnn@gmail.com>
2022-09-03 17:23:28 +0700

    release to pypi at https://pypi.org/project/BMKG-Latest/0.1/

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: Microsoft :: Windows",
     ],
     packages=setuptools.find_packages(),
